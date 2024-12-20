f8e8e8bb14fb986b43ce21c996b4d2af8ee4fa2d
bharathikannansambasivam <bharathikannansambasivam@gmail.com>
2024-12-20 20:42:10 +0530

    feat: PWA feature added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: "./public/manifest.json",
+      registerType: "autoUpdate",
+    }),
+  ],
   server: {
     port: 3001,
   },
